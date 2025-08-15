d092c6f5c7620ac2b2b3f8dfa7f205a73a13b909
Neftalem <dneftalem@gmail.com>
2025-08-15 13:10:22 +0300

    finshed the viwe stydent

--- index.py
+++ index.py
@@ -1,18 +1,29 @@
 import os
 
-Fill_NAME = "gread.txt"
+Fill_NAME = "grades.txt"
 
 def add_student():
 
     name = input("Enter Your Name: ")
     score = input("Enter Your Score: ")
-    score_list = [int(s.strip()) for s in score.split(',')]
+    score_list = [int(s.strip()) for s in score.split(',')] # the score change to int use for
 
     avarage = sum(score_list) / len(score_list)
 
     with open("Fill_NAME", "a") as f:
-        f.write(f"{name}: {",".join(map(str, score_list))}:{avarage:.2f}\n")
+        f.write(f"{name}: {",".join(map(str, score_list))}:{avarage:.2f}\n") # change the int to str with .join
 
-        print(f"{name} Your Score {avarage:.2f}")
+        print(f"{name} Your Score {avarage:.2f}") # .2f is float
+def viwe_student():
+    if not os.path.exists(Fill_NAME): # not os.path.exists the score dose note in the fill
+        print(f"No student data not founf")
+        return
+    with open(Fill_NAME, "r") as f:
+        lines = f.readlines()
 
-add_student()
+    print("\n Student Recourd")
+
+    for line in lines:
+        name, score, ave = line.strip().split(':')
+        print(f"{name} Score:{score} avarage:{ave}")
+viwe_student()

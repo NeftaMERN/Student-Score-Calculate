d15e7b51c5548f742b4be53aed03dab4e2dfb3d8
Neftalem <dneftalem@gmail.com>
2025-08-15 12:30:45 +0300

    finsh add student

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
 import os
 
 Fill_NAME = "gread.txt"
+
+def add_student():
+
+    name = input("Enter Your Name: ")
+    score = input("Enter Your Score: ")
+    score_list = [int(s.strip()) for s in score.split(',')]
+
+    avarage = sum(score_list) / len(score_list)
+
+    with open("Fill_NAME", "a") as f:
+        f.write(f"{name}: {",".join(map(str, score_list))}:{avarage:.2f}\n")
+
+        print(f"{name} Your Score {avarage:.2f}")
+
+add_student()
